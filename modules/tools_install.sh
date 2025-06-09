4610aef634685bccecb2f7a8eddcdb957f308fc5
1443213244 <1443213244@qq.com>
2025-06-09 17:01:48 +0800

    Update tools_install.sh

--- modules/tools_install.sh
+++ modules/tools_install.sh
@@ -1,41 +1,10 @@
 #!/bin/bash
 
-# 工具列表
-TOOLS=(
-    # 网络工具
-    "net-tools"
-    "iproute2"
-    "curl"
-    "wget"
-    "dnsutils"
-    
-    # 系统工具
-    "htop"
-    "iotop"
-    "sysstat"
-    "vim"
-    "git"
-    
-    # 安全工具
-    "fail2ban"
-    "ufw"
-    "iptables"
-    
-    # 监控工具
-    "prometheus-node-exporter"
-    "prometheus"
-    
-    # VPN 工具
-    "wireguard-tools"
-)
-
 # 安装工具
 install_tools() {
     echo "正在安装工具..."
-    for tool in "${TOOLS[@]}"; do
-        echo "安装 $tool..."
-        apt-get install -y "$tool"
-    done
+    curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+    apt-get install net-tools iproute2 curl wget iftop wireguard-tools iptables speedtest -y
     
     echo "清理不需要的包..."
     apt-get autoremove -y
